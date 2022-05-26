b4836bc1004863a31eacee4cf35f708b11c60d82
Ariel Farbiarz <arielfarbiarz@gmail.com>
2022-05-26 19:29:41 -0300

    Some tests

--- prueba/src/App.js
+++ prueba/src/App.js
@@ -1,10 +1,16 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import Componente from './Components/Componente';
+import Propiedades from './Components/Propiedades';
+import Estado from './Components/Estado';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <section>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,6 +23,23 @@ function App() {
         >
           Learn React
         </a>
+        </section>
+        <section>
+        <Componente msg="Hola soy un Componente Funcional expresado desde una prop"/>
+        <hr/>
+        <Propiedades 
+        cadena="Esto es una cadena de texto" 
+        numero={19}
+        booleano = {false}
+        arreglo = {[1,2,3]} 
+        objeto = {{nombre: 'Ariel', correo: 'arielfarbiarz@gmail.com'}}
+        funcion = {(num) => num * num}
+        elementoReact ={<i>Esto es un elemento React</i>}
+        componenteReact = {<Componente msg = "Soy un componente pasado como Prop"/>}
+        />  
+        <hr/>
+        <Estado />
+      </section>
       </header>
     </div>
   );
